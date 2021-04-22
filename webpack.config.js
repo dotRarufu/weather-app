4fed91586f889e1629d5542cb4efed244a9cd60b
skrr-tech <81721522+skrr-tech@users.noreply.github.com>
2021-04-22 12:56:02 +0800

    more fixes

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,14 @@ module.exports = {
                 test: /\.scss$/,
                 use: ['style-loader', 'css-loader', 'sass-loader'],
             },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg)$/,
+                use: ['url-loader'],
+            },
         ],
     },
     resolve: {
